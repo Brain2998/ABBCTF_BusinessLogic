f84e962001ec3987129b06d17575406461788a95
Серебряков Андрей Дмитриевич <SerebryakovAD@moffice.akbars.ru>
2019-11-26 18:00:52 +0300

    hashing and encoding

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import equations
 import base64
 from flask_session import Session
 import json
+from urllib import parse
+from Cryptodome.Hash import SHA256
 
 app = flask.Flask(__name__)
 
@@ -12,16 +14,24 @@ flag="Abbflagis{[A-z0-9_]*}"
 @app.route('/', methods = ['GET', 'POST'])
 def root():
     if flask.request.method=='POST':
-        formData=flask.request.form
-        flask.session['tasksCount']=int(formData['taskNumber'])
-        if (int(formData['x'])==flask.session['solutions'][0] and int(formData['y'])==flask.session['solutions'][1] and int(formData['z'])==flask.session['solutions'][2]):
-            if (flask.session['tasksCount']==10000):
-                return flask.Response(json.dumps({'result': True, 'tasksToSolve': 10000-flask.session['tasksCount'], 'flag': flag}))
+        stringData=base64.b64decode(flask.request.data).decode('utf-8')
+        dictData=dict(parse.parse_qsl(stringData))
+        params=stringData[:stringData.index('&checkSum')]
+        #check hash of request
+        if (SHA256.new(params.encode('utf-8')).hexdigest() == dictData['checkSum']):
+            flask.session['tasksCount']=int(dictData['taskNumber'])
+            #is result right
+            if (int(dictData['x'])==flask.session['solutions'][0] and int(dictData['y'])==flask.session['solutions'][1] and int(dictData['z'])==flask.session['solutions'][2]):
+                #if task number is 10000 return the flag
+                if (flask.session['tasksCount']==10000):
+                    return flask.Response(json.dumps({'result': True, 'tasksToSolve': 10000-flask.session['tasksCount'], 'flag': flag}))
+                else:
+                    flask.session['tasksCount']+=1
+                    return flask.Response(json.dumps({'result': True, 'tasksToSolve': 10000-flask.session['tasksCount']}))
             else:
-                flask.session['tasksCount']+=1
-                return flask.Response(json.dumps({'result': True, 'tasksToSolve': 10000-flask.session['tasksCount']}))
+                return flask.Response(json.dumps({'result': False, 'tasksToSolve': 10000-flask.session['tasksCount']}))
         else:
-            return flask.Response(json.dumps({'result': False, 'tasksToSolve': 10000-flask.session['tasksCount']}))
+            return flask.Response(status=400)
     return flask.send_from_directory('.', 'static/index.html')
 
 @app.route('/js/<path:path>')
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
     generateEquation()
     var taskNumber=0
-    $('form').submit(function(e){
+    $('form').submit(async function(e){
+        e.preventDefault();
         var formData=new FormData($('form')[0])
         formData.set('taskNumber',taskNumber)
+        var urlFormData=new URLSearchParams(formData).toString();
+        var encoder=new TextEncoder();
+        var encodedData=encoder.encode(urlFormData)
+        var hash=await crypto.subtle.digest('SHA-256', encodedData)
+        const hashArray = Array.from(new Uint8Array(hash));
+        const hashDigest = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+        urlFormData+='&checkSum='+hashDigest
         $.ajax({
             async:true,
             type: "POST",
             url: "/",
-            data: formData,
+            data: btoa(urlFormData),
             cache: false,
 			contentType: false,
             processData: false,
@@ -22,9 +30,12 @@ $(document).ready(function() {
                     alert(`${resultJSON.result? 'Right': 'Wrong'}! Need to solve ${resultJSON.tasksToSolve} more tasks.`)
                     generateEquation()
                 }
+            },
+            error: function(err){
+                alert(`ajax err: ${JSON.stringify(err,null,2)}`);
             }
         });
-        e.preventDefault();
+        
     });
     function generateEquation() {
         $.ajax({
@@ -33,6 +44,9 @@ $(document).ready(function() {
             url: "/generateEquation",
             success: function(image){
                 $('#equation').attr('src',"data:image/png;base64,"+image)
+            },
+            error: function(err){
+                alert(`ajax err: ${JSON.stringify(err,null,2)}`);
             }
         });
     }
